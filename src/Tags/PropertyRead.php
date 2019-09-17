6adea0679bd66da0a6b4cf3df3826c934f9e167d
Tom Sartain <tomsartain@gmail.com>
2019-09-17 00:59:40 -0400

    add PropertyRead

---
+++ src/Tags/PropertyRead.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace xiian\docgenerator\Tags;
+
+class PropertyRead extends \phpDocumentor\Reflection\DocBlock\Tags\PropertyRead implements Annotatable
+{
+    use AnnotationTrait;
+}
