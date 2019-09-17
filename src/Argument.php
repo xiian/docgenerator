67164c3a5da9cd5b60eb23fbc4a16d5faa2ec71a
Tom Sartain <tomsartain@gmail.com>
2019-09-17 19:16:03 -0400

    Create Argument object

---
+++ src/Argument.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace xiian\docgenerator;
+
+use phpDocumentor\Reflection\Type;
+
+class Argument
+{
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var Type
+     */
+    protected $type;
+
+    /**
+     * @var string
+     */
+    protected $defaultValue;
+
+    public function __construct(string $name, Type $type, string $defaultValue = null)
+    {
+        $this->name         = $name;
+        $this->type         = $type;
+        $this->defaultValue = $defaultValue;
+    }
+
+    public function __toString()
+    {
+        $out = ((string) $this->type) . ' $' . $this->name;
+
+        if ($this->defaultValue) {
+            $out .= ' = ' . $this->defaultValue;
+        }
+
+        return trim($out);
+    }
+}
