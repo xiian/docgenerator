626c1a5d7c106eefe5ef104eda1899a44ef597c1
Tom Sartain <tomsartain@gmail.com>
2019-09-17 00:59:47 -0400

    add PropertyWrite

---
+++ src/Tags/PropertyWrite.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace xiian\docgenerator\Tags;
+
+class PropertyWrite extends \phpDocumentor\Reflection\DocBlock\Tags\PropertyWrite implements Annotatable
+{
+    use AnnotationTrait;
+}
